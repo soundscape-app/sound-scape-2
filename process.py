a9fccc5cdf612eb784227f4b1fb753bb3acaeb5a
2020056608 <2020056608@hanyang.ac.kr>
2022-07-12 20:07:29 +0900

    process.py soundscape2 model 적용

--- process.py
+++ process.py
@@ -17,6 +17,33 @@ import requests
 import json
 import math
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn.preprocessing import MinMaxScaler
+
+class MultOutRegressor(nn.Module):
+    def __init__(self, input_dim, target_dim, hidden_dim=32,seed=1234):
+        super().__init__()
+        torch.manual_seed(seed)
+        self.hidden_dim = hidden_dim
+        self.target_dim = target_dim
+
+        self.fc1 = nn.Linear(input_dim, self.hidden_dim)
+        self.fc2 = nn.Linear(self.hidden_dim, self.hidden_dim)
+        self.fc3 = nn.Linear(self.hidden_dim, self.target_dim)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        return x
+
+model = MultOutRegressor(6, 5)
+model.load_state_dict(torch.load('./model_state_dict.pt'))
+
 def proc(video):
     URL = 'http://api-server-1507785389.ap-northeast-2.elb.amazonaws.com:4040/process'
     response = requests.post(URL, files={'video': video.video.file})
@@ -58,23 +85,32 @@ def revisitation(data):
     result = sum([CF[key] * data[key] for key in CF.keys()]) + CONST
     return result
 
-def predict(data):
-    CF = {
-        'laeq': 0.051,
-        'lceq-laeq': -0.166,
-        'green_ratio': 0.058,
-        'n_ppl': -0.031,
-        'n_vhcl': -0.018,
-        'loudness': -0.57,
-        'revisitation': 0.815,
-    }
-    CONST = -1.568
+# def predict(data):
+#     CF = {
+#         'laeq': 0.051,
+#         'lceq-laeq': -0.166,
+#         'green_ratio': 0.058,
+#         'n_ppl': -0.031,
+#         'n_vhcl': -0.018,
+#         'loudness': -0.57,
+#         'revisitation': 0.815,
+#     }
+#     CONST = -1.568
     
-    result = sum([CF[key] * data[key] for key in CF.keys()]) + CONST
-    exp_result = math.exp(result)
-    percent = exp_result / (1 + exp_result)
-    return percent
+#     result = sum([CF[key] * data[key] for key in CF.keys()]) + CONST
+#     exp_result = math.exp(result)
+#     percent = exp_result / (1 + exp_result)
+#     return percent
 
+def predict(data):
+    feature = ["LAeq", "LA5-95","Loudness", "Green", "Sky", "Grey", ]
+    input = []
+    for f in feature:
+        input.append(data[f])
+    res = model(torch.FloatTensor(input))
+    sum_res = res[0]+res[1]+res[2]+res[3]+res[4]+1.568
+    m = nn.Sigmoid()
+    return m(sum_res).item()
 
 @permission_classes((AllowAny,))
 class ProcessViewSet(viewsets.ViewSet):
